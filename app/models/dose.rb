90f650f2bd20ad486e048cf1d2c4ded9951b16b8
Vivian Salles <vivianmsalles@gmail.com>
2020-08-13 11:56:08 +0200

    finished models

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,10 @@
 class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
+
+  validates :description, presence: true
+  validates :cocktail, presence: true
+  validates :cocktail, uniqueness: { scope: :ingredient }
+  validates :ingredient, presence: true
+
 end
